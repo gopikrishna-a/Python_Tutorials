72e955b275fac0319c40295f56a1836e1843282a
Gopikrishna <a.gopikrishna01@gmail.com>
2017-06-24 15:42:02 +0530

    Create if_elif.py

---
+++ if_elif.py
@@ -0,0 +1,13 @@
+name = "Gopikrishna"
+
+if name is "Lucky":
+    print "Hi Lucky"
+elif name is "Gopikrishna":
+    print "Hi Gopikrishna"
+elif name is "Bucky":
+    print "Hi Bucky"
+else:
+    print "Please Sign Up"
+
+
+    
