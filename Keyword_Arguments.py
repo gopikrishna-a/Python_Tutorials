c18d722b42925e4ef82c45c7254ff9e9f7aa8312
Gopikrishna <a.gopikrishna01@gmail.com>
2017-07-04 11:43:25 +0530

    Create Keyword_Arguments.py

---
+++ Keyword_Arguments.py
@@ -0,0 +1,7 @@
+def dumb_sentence(name='Gopi',action='ate',item='Tuna'):
+    print name,action,item
+
+dumb_sentence()
+dumb_sentence(name='Anand')
+dumb_sentence(item='Apple')
+dumb_sentence(action='is',item='Awesome')
