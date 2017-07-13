54f4c33099a9cc9d354ad73262f8f3cdeb9d3249
Gopikrishna <a.gopikrishna01@gmail.com>
2017-07-13 18:39:25 +0530

    Create 30__init__.py

---
+++ 30__init__.py
@@ -0,0 +1,10 @@
+class Tuna:
+    def __init__(self):
+        print "Hello World"
+    def swim(self):
+        print "I am Swimming"
+
+flipper = Tuna()
+flipper.swim()
+"""The __init__ function is called a constructor, or initializer, and is automatically called,
+when you create a new instance of a class. Within that function, the newly crea
