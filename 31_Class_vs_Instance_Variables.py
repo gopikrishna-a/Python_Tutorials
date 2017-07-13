47abb916279fbfd737d57429cd59cd556abcf8a9
Gopikrishna <a.gopikrishna01@gmail.com>
2017-07-13 18:50:23 +0530

    Create 31_Class_vs_Instance_Variables.py

---
+++ 31_Class_vs_Instance_Variables.py
@@ -0,0 +1,14 @@
+class Girl:
+    gender = 'female'
+
+    def __init__(self,name):
+        self.name = name
+
+
+r = Girl('Richel')
+s = Girl('Stanky')
+
+print r.name
+print r.gender
+print s.name
+print s.gender
