5c90e8dea3803b667f2dac8fe8d02bccb43a40b2
Gopikrishna <a.gopikrishna01@gmail.com>
2017-07-13 18:45:06 +0530

    Update 30__init__.py

--- 30__init__.py
+++ 30__init__.py
@@ -1,3 +1,8 @@
+"""The __init__ function is called a constructor, or initializer, and is automatically called 
+when you create a new instance of a class. Within that function, the newly created object is 
+assigned to the parameter self ."""
+
+#Example One
 class Tuna:
     def __init__(self):
         print "Hello World"
@@ -6,5 +11,19 @@ class Tuna:
 
 flipper = Tuna()
 flipper.swim()
-"""The __init__ function is called a constructor, or initializer, and is automatically called,
-when you create a new instance of a class. Within that function, the newly crea
+
+
+
+#Example Two
+
+class Enemy:
+    def __init__(self,x):
+        self.energy = x
+    def get_energy(self):
+        print self.energy
+
+jason = Enemy(5)
+sandy = Enemy(18)
+
+jason.get_energy()
+sandy.get_energy()
