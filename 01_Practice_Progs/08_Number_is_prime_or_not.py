f91dfb2a3927f47f838ace18f75f66646e50e021
Gopikrishna <a.gopikrishna01@gmail.com>
2017-09-05 11:18:26 +0530

    Update 08_Number_is_prime_or_not.py

--- 01_Practice_Progs/08_Number_is_prime_or_not.py
+++ 01_Practice_Progs/08_Number_is_prime_or_not.py
@@ -8,3 +8,19 @@ def is_prime(num):
 while True:
     num = int(raw_input("Enter Ur Number : "))
     is_prime(num)
+    
+    
+    
+    
+    
+    
+    
+    
+#Without function
+num = 13
+for i in range(2,num):
+    if num % i ==0:
+        print "Not a Prime"
+        break
+else:
+    print "Prime"
