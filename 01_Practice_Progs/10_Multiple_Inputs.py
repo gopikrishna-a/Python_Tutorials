c622ef503691d287fd13030f27c4b1804a3756db
Gopikrishna <a.gopikrishna01@gmail.com>
2017-09-06 11:14:18 +0530

    Update 10_Multiple_Inputs.py

--- 01_Practice_Progs/10_Multiple_Inputs.py
+++ 01_Practice_Progs/10_Multiple_Inputs.py
@@ -5,3 +5,14 @@ n = int(raw_input().strip())
 arr = map(int,raw_input().strip().split(','))
 
 print arr
+
+
+
+
+
+
+n = int(raw_input("Plase enter number of inputs :").strip())
+arr = map(int,raw_input("Enter ur input numbers saperated by white space : ").strip().split(' '))
+while len(arr) != n:
+    arr = map(int,raw_input("Oops no. of inputs not matched Enter again: ").strip().split(' '))
+print arr
