159cd49d99a2e87596fc8db40a03f864f9e1565c
Gopikrishna <a.gopikrishna01@gmail.com>
2017-09-07 11:49:10 +0530

    Update id_9_Triangles.py

--- CODE_ABBEY/id_9_Triangles.py
+++ CODE_ABBEY/id_9_Triangles.py
@@ -13,7 +13,7 @@ A,B,C are sides of a triangle
 n = int(raw_input("Enter No. of Inputs : ").strip())
 l = []
 for i in range(0,n):
-    arr = map(float,raw_input("Enter The weight and Height saperated by space : ").strip().split(" "))
+    arr = map(float,raw_input("Enter The sides of the triangle saperated by space : ").strip().split(" "))
     tr = 0
     if ((arr[0] + arr[1]) > arr[2]) and ((arr[1] + arr[2]) > arr[0]) and ((arr[0] + arr[2]) > arr[1]):
         tr = 1
