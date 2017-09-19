8a621723012a759d3040ddadc789fa91aa4daf06
Gopikrishna <a.gopikrishna01@gmail.com>
2017-09-19 18:42:01 +0530

    Create 002_Creating_a_table_in_db.py

---
+++ 02_PYTHON_MYSQLDB/002_Creating_a_table_in_db.py
@@ -0,0 +1,21 @@
+import MySQLdb
+
+
+db = MySQLdb.connect('localhost','root','asm123','TESTDB')
+cursor = db.cursor()
+
+#Try to create the STUDENTS table if not daabase
+try:
+    sql = """CREATE TABLE STUDENTS (
+             NAME CHAR(20),
+             AGE INT,
+             COURSE CHAR(20));"""
+    cursor.execute(sql)
+
+except:
+    print "Table STUDENTS already created"
+    
+else:
+    print "Table created Successfully"
+
+db.close()
