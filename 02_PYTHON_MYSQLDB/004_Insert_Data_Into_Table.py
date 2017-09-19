5655add3bb9375666e5744813c84224d311c399c
Gopikrishna <a.gopikrishna01@gmail.com>
2017-09-19 19:13:59 +0530

    Create 004_Insert_Data_Into_Table.py

---
+++ 02_PYTHON_MYSQLDB/004_Insert_Data_Into_Table.py
@@ -0,0 +1,17 @@
+import MySQLdb
+
+
+db = MySQLdb.connect('localhost','root','asm123','TESTDB')
+cursor = db.cursor()
+
+
+sql = "INSERT INTO STUDENTS(NAME,AGE,COURSE) VALUES ('%s', '%d', '%s'),('%s', '%d', '%s'),('%s', '%d', '%s');" % ('Gopikrishna', 23, 'ECE','Gopikrishna', 23, 'ECE','Gopikrishna', 23, 'ECE')
+
+cursor.execute(sql)
+
+cursor.execute("select * from STUDENTS;")
+data = cursor.fetchall()
+print "================================="
+print data
+db.commit()
+db.close()
