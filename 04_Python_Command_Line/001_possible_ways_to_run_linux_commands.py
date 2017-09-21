0b7e871ad64eaf5c629d93086a4593315bb075e7
Gopikrishna <a.gopikrishna01@gmail.com>
2017-09-21 12:27:17 +0530

    Update and rename Untitled Document 1 to 001_possible_ways_to_run_linux_commands.py

---
+++ 04_Python_Command_Line/001_possible_ways_to_run_linux_commands.py
@@ -0,0 +1,28 @@
+#Method --> 1
+
+import os
+
+os.system('ls)
+
+
+
+#Method --> 2
+
+from subprocess import call
+
+call('ls')
+
+
+# Method --> 3
+
+>>> import subprocess
+>>> cmd = subprocess.check_output('ls')
+>>> print cmd
+
+"""
+Conclusion:
+
+the subprocess module, which provides much flexibility when working with Unix commands through its different functions. 
+
+"""
+
