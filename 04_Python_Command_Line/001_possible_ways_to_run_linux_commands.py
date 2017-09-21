bb2e22189de7373eda1213e3e21e4fea600f7f2d
Gopikrishna <a.gopikrishna01@gmail.com>
2017-09-21 12:31:41 +0530

    Update 001_possible_ways_to_run_linux_commands.py

--- 04_Python_Command_Line/001_possible_ways_to_run_linux_commands.py
+++ 04_Python_Command_Line/001_possible_ways_to_run_linux_commands.py
@@ -15,9 +15,19 @@ call('ls')
 
 # Method --> 3
 
->>> import subprocess
->>> cmd = subprocess.check_output('ls')
->>> print cmd
+import subprocess
+
+cmd = subprocess.check_output('ls')
+print cmd
+
+
+
+# Method --> 4
+
+import subprocess 
+time = subprocess.Popen('ls', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output, err = time.communicate()
+print output
 
 """
 Conclusion:
