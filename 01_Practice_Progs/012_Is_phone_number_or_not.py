89f0be471f0a4e2719c90fa5f53332d6981ccdd2
Gopikrishna <a.gopikrishna01@gmail.com>
2017-10-05 16:41:38 +0530

    Create 012_Is_phone_number_or_not.py

---
+++ 01_Practice_Progs/012_Is_phone_number_or_not.py
@@ -0,0 +1,35 @@
+
+#With out regx
+#number format = '415-555-1234'
+
+
+def isphonenumber(txt):
+    if len(txt) != 12:
+        return False
+    for i in range(0,3):
+        if not txt[i].isdecimal():
+            return False
+    if txt[3] != '-':
+        return False
+    for i in range(4,7):
+        if not txt[i].isdecimal():
+            return False
+    if txt[7] != '-':
+        return False
+    for i in range(8,12):
+        if not txt[i].isdecimal():
+            return False
+    return True
+
+
+
+message = 'call me on this number 415-555-1234 or call to this 415-565-1234 phone number'
+numberfound= False
+
+for i in range(len(message)):
+    chunk =  message[i:i+12]
+    if isphonenumber(chunk):
+        print( 'phone number found ' + chunk)
+        numberfound = True
+if not numberfound:
+    print( 'could not found any phone numbers')
