49d78825a29e1ad6deafbf605a0c60931dc0c07c
Gopikrishna <a.gopikrishna01@gmail.com>
2017-10-13 15:20:31 +0530

    Update 002_drawing_square.py

--- 06_Python_turtle/002_drawing_square.py
+++ 06_Python_turtle/002_drawing_square.py
@@ -1,6 +1,19 @@
 import turtle
 
 
+#creating function to draw a square
+def draw_square(t):
+  for i in range(4):
+    t.fd(100)
+    t.lt(90)  #value in degree (arrow movement direction)
+
+bob = turtle.Turtle()
+draw_square(bob)
+
+#output wil be a square diagram
+
+
+
 
 
 
