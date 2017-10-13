dd28b6f20ba59d269f43798d6e6c14f9fb02ad96
Gopikrishna <a.gopikrishna01@gmail.com>
2017-10-13 15:10:42 +0530

    Update 001_introduction_to_turtle_module.py

--- 06_Python_turtle/001_introduction_to_turtle_module.py
+++ 06_Python_turtle/001_introduction_to_turtle_module.py
@@ -1,8 +1,11 @@
+#The turtle module:
+##################
+#the turtle modules allows you to create images using turtle graphics.
+#To check whether you have the turtle module, open the Python interpreter and type
 
-sdfkjdg
-sdfkjdg
-sdfkjdg
-sdfkjdg
-sdfkjdg
-sdfkjdg
+import turtle
+bob = turtle.Turtle()
+
+#When you run this code, it should create a new window with small arrow that represents the turtle.
+
 
