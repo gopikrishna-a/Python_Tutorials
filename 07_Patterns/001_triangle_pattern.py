7298a491bb0c61e9cece1ae8eff6f67be8765b2c
Gopikrishna <a.gopikrishna01@gmail.com>
2017-10-23 19:12:09 +0530

    Update 001_triangle_pattern.py

--- 07_Patterns/001_triangle_pattern.py
+++ 07_Patterns/001_triangle_pattern.py
@@ -12,12 +12,39 @@ Output:
 
 """
 
-
+#code for pattern: 1
 def print_triangle(num):
     cnt = num
     for i in range(num):
         print('#' * cnt)
         cnt -= 1
+    return " Pattern printed"
+    
+    
+    
+num = int(input('Enter ur input number: '))
+print(print_triangle(num))
+
+
+
+#code for pattern: 2
+"""
+input: 5
+out put:
+    
+#
+##
+###
+####
+#####
+
+"""
+
+def print_triangle(num):
+    cnt = 1
+    for i in range(num,0,-1):
+        print('#' * cnt)
+        cnt += 1
     return " Pattern printed"
     
     
