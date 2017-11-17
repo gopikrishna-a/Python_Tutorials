0c931870f7b3b85b7f916de9832841cd054f46ef
Gopikrishna <a.gopikrishna01@gmail.com>
2017-11-17 15:35:31 +0530

    Create 000_upgrade_python_linux.sh

---
+++ 000_upgrade_python_linux.sh
@@ -0,0 +1,9 @@
+Link:  https://www.tecmint.com/install-python-in-linux/
+
+
+# wget https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tar.xz
+# tar xJf Python-3.6.3.tar.xz
+# cd Python-3.6.3
+# ./configure
+# make
+# make install
