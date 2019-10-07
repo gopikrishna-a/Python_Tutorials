9e48045a346d9e4c0ba6c8cdd0188058eecd0d54
Gopikrishna Arepalli <a.gopikrishna01@gmail.com>
2019-10-07 11:42:31 +0530

    Update 002_ipv4_pattern.py

--- 003_REGULAR_EXPRESSIONS/002_ipv4_pattern.py
+++ 003_REGULAR_EXPRESSIONS/002_ipv4_pattern.py
@@ -6,3 +6,16 @@ pattern = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
 
 
 print(re.findall(pattern,text))
+
+
+
+# The above regx will match all valid and invalid IPv4 address hence
+
+
+
+import re
+
+text = 'this is my sss333 333email id 10.35.4.216 hagsdvjksbd 5246 jbd 8888881 10.20.21.5 jdfn 256.67.89.65'
+obj = re.findall(r'\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-5][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-9][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-9][0-9]|[01]?[0-9][0-9]?)\b'), text)
+print(obj)
+
